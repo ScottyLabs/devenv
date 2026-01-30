0634fde80298f1b73085c622395597dd24fbdeb9
Anish Pallati <i@anish.land>
2026-01-30 14:04:01 -0500

    feat: remove formatter and nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,17 +1,7 @@
 {
   description = "ScottyLabs shared devenv configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self }: {
+    devenvModules.default = import ./modules;
   };
-
-  outputs =
-    { self, nixpkgs }:
-    {
-      devenvModules.default = import ./modules;
-
-      formatter = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (
-        system: nixpkgs.legacyPackages.${system}.nixfmt-tree
-      );
-    };
 }
