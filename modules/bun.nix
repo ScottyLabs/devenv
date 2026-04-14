afb4dc7da1e243bb18d4a15d69c54d7342cea9c9
Anish Pallati <i@anish.land>
2026-04-14 16:53:18 -0400

    fix: configure oxfmt to use 4-space indentation

--- modules/bun.nix
+++ modules/bun.nix
@@ -12,6 +12,17 @@ in
     packages = with pkgs; [ bun ];
 
     treefmt.config.programs.oxfmt.enable = true;
+    treefmt.config.settings.formatter.oxfmt.options = [
+      "--config"
+      (toString (pkgs.writeText "oxfmtrc.json" (builtins.toJSON {
+        tabWidth = 4;
+        useTabs = false;
+        overrides = [{
+          files = [ "**/*.yaml" "**/*.yml" "**/*.html" ];
+          options.tabWidth = 2;
+        }];
+      })))
+    ];
 
     git-hooks.hooks.oxlint = {
       enable = true;
